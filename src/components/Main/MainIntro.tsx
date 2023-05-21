a8919694b24d497eed6d461161eea646b00e680c
kimdonggu42 <kdonggoo44@gmail.com>
2023-05-21 16:03:55 +0900

    Design : 메인페이지의 지도 이동 버튼 hover 효과 변경

--- src/components/Main/MainIntro.tsx
+++ src/components/Main/MainIntro.tsx
@@ -17,12 +17,54 @@ const MainWrapper = styled.div`
   width: 400px;
   padding: 20px;
   margin-right: 20px;
-  /* border: 1px solid red; */
+`;
 
-  > :not(:second-child) {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+const InnerMoveMapBtn = styled.div`
+  position: relative;
+  width: 200px;
+  height: 50px;
+  margin-top: -37px;
+  background: #55b290;
+  left: -250px;
+  transition: 0.3s;
+
+  > .btnText2 {
+    padding-top: 13px;
+    font-size: 19px;
+    font-weight: 700;
+    margin-right: -130px;
+    color: white;
+  }
+`;
+
+const OuttetMoveMapBtn = styled.button`
+  background: white;
+  /* margin-top: 30px; */
+  width: 330px;
+  height: 50px;
+  overflow: hidden;
+  transition: 0.2s;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+
+  > .btnText {
+    margin-top: 13px;
+    font-size: 19px;
+    font-weight: 700;
+    color: #2a2c32;
+    transition: 0.3s;
+  }
+
+  &:hover .btnText {
+    margin-left: 65px;
+  }
+
+  &:hover ${InnerMoveMapBtn} {
+    left: -130px;
+  }
+  &:hover ${IntroContainer} {
+    background-color: white;
   }
 `;
 
@@ -36,33 +78,16 @@ const MainTitle = styled.div`
 `;
 
 const SubTitle = styled.div`
-  margin-top: 20px;
+  margin: 15px 0 30px 0;
   color: white;
   font-size: 17px;
-`;
-
-const MoveMapBtn = styled.button`
-  color: #2a2c32;
-  font-size: 19px;
-  font-weight: 700;
-  margin-top: 30px;
-  width: 330px;
-  height: 50px;
-  border-radius: 10px;
-  border: none;
-  background-color: white;
-  cursor: pointer;
-
-  &:hover {
-    background-color: #f1f3f5;
-  }
+  margin-right: 120px;
 `;
 
 const PhoneImgWrapper = styled.div`
   display: flex;
   justify-content: center;
   margin-left: 20px;
-  /* border: 1px solid red; */
 
   > img {
     width: 400px;
@@ -82,14 +107,19 @@ function MainIntro() {
           save<span>me</span>
         </MainTitle>
         <SubTitle>
-          별도의 검색 필요없이 <br />
-          바로 내 주변의 화장실을 찾아보세요.
+          별도의 검색 필요없이 바로
+          <br />내 주변의 화장실을 찾아보세요.
         </SubTitle>
         <Link to='Main'>
-          <MoveMapBtn>내 주변의 화장실 찾기</MoveMapBtn>
+          <OuttetMoveMapBtn>
+            <p className='btnText'>내 주변의 화장실 찾기</p>
+            <InnerMoveMapBtn>
+              <p className='btnText2'>GO!</p>
+            </InnerMoveMapBtn>
+          </OuttetMoveMapBtn>
         </Link>
       </MainWrapper>
-      <PhoneImgWrapper>
+      <PhoneImgWrapper className='test'>
         <img src={mainImg} alt='메인 페이지 모바일 이미지' />
       </PhoneImgWrapper>
     </IntroContainer>
