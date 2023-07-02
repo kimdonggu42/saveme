b68883c8cb75de61751deb251fad6ace83d6cec4
kimdonggu42 <kdonggoo44@gmail.com>
2023-07-02 16:11:48 +0900

    Refact : 기존 App 컴포넌트에 있던 사용자의 현재 위치 불러오는 코드 부분을 Intro 컴포넌트로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,9 @@
 import Intro from "./pages/Intro";
 import MainMap from "./pages/MainMap";
 import GlobalStyle from "./assets/style/globalStyle";
-import { useEffect } from "react";
-import { currentMyLocationAtom } from "./recoil/atom";
-import { useSetRecoilState } from "recoil";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const setCurrentMyLocation = useSetRecoilState(currentMyLocationAtom);
-
-  // 내 현재 위치 계산
-  useEffect(() => {
-    // 내 현재 위치 값 번환 성공 시 실행 함수 -> 내 현재 위치 값을 currentMyLocationAtom에 저장
-    const success = (location: { coords: { latitude: number; longitude: number } }) => {
-      setCurrentMyLocation({
-        lat: location.coords.latitude,
-        lng: location.coords.longitude,
-      });
-    };
-    // 내 현재 위치 값 반환 실패 시 실행 함수 -> 지도 중심을 서울시청 위치로 설정
-    const error = () => {
-      setCurrentMyLocation({ lat: 37.5666103, lng: 126.9783882 });
-    };
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(success, error);
-    }
-  }, [setCurrentMyLocation]);
-
   return (
     <div className='App'>
       <GlobalStyle />
--- src/pages/Intro.tsx
+++ src/pages/Intro.tsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { currentMyLocationAtom } from "../recoil/atom";
+import { useSetRecoilState } from "recoil";
 import mainImg from "../assets/images/mainImg.png";
 
 const IntroContainer = styled.div`
@@ -103,6 +106,26 @@ const PhoneImgWrapper = styled.div`
 `;
 
 function Intro() {
+  const setCurrentMyLocation = useSetRecoilState(currentMyLocationAtom);
+
+  // 내 현재 위치 계산
+  useEffect(() => {
+    // 내 현재 위치 값 번환 성공 시 실행 함수 -> 내 현재 위치 값을 currentMyLocationAtom에 저장
+    const success = (location: { coords: { latitude: number; longitude: number } }) => {
+      setCurrentMyLocation({
+        lat: location.coords.latitude,
+        lng: location.coords.longitude,
+      });
+    };
+    // 내 현재 위치 값 반환 실패 시 실행 함수 -> 지도 중심을 서울시청 위치로 설정
+    const error = () => {
+      setCurrentMyLocation({ lat: 37.5666103, lng: 126.9783882 });
+    };
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  }, [setCurrentMyLocation]);
+
   return (
     <IntroContainer>
       <MainWrapper>
