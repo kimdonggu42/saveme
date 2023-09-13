7d0289091d1f40ece06a15222b059b7424fb0570
kimdonggu42 <kdonggoo44@gmail.com>
2023-09-13 20:18:28 +0900

    chore: recoil root 제거

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { RecoilRoot } from "recoil";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  // <React.StrictMode>
-  <RecoilRoot>
+  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </RecoilRoot>
-  // </React.StrictMode>
+  </React.StrictMode>
 );
