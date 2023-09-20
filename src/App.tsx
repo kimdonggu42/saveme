442f05775befdce2c319124526cb11763244b2c8
kimdonggu42 <kdonggoo44@gmail.com>
2023-09-20 18:56:26 +0900

    chore: App component에서 Spinner 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import "./assets/style/globals.css";
 import Router from "./Router";
-import Spinner from "./components/Spinner";
 
 export default function App() {
   return (
     <>
-      <Spinner />
       <Router />
     </>
   );
