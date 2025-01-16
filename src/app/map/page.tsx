149923f17b17d33d5832c87dc8dec6eac1c82d10
kimdonggu42 <kdk3280@undermilli.com>
2025-01-16 22:43:09 +0900

    feat: 내 주변 100개의 화장실이 아닌 서울 전 지역 화장실을 지도에 표시하도록 수정

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -18,21 +18,20 @@ export default function MainMap() {
 
   const { lat, lng } = currentMyCoordinates;
 
-  // 화장실 데이터 중 가까운 순으로 100개만 추출
-  const filterdToilets = toilets
-    .map((item) => {
-      const distance = distanceCalculation(lat, lng, item.Y_WGS84, item.X_WGS84, 'K');
-      return { ...item, DISTANCE: distance };
-    })
-    .sort((a, b) => a.DISTANCE - b.DISTANCE)
-    .filter((_, index) => index < 100);
+  // 가장 가까운 화장실 찾기
+  const [closestToilet] = toilets
+    .map((item) => ({
+      ...item,
+      DISTANCE: distanceCalculation(lat, lng, item.Y_WGS84, item.X_WGS84, 'K'),
+    }))
+    .sort((a, b) => a.DISTANCE - b.DISTANCE);
 
   // 지도 초기화
   useEffect(() => {
     if (lat !== 0 && lng !== 0) {
       mapRef.current = new naver.maps.Map('map', {
         center: new naver.maps.LatLng(lat, lng),
-        zoom: 15,
+        zoom: 18,
         minZoom: 12,
         zoomControl: true,
         mapTypeControl: true,
@@ -54,11 +53,11 @@ export default function MainMap() {
         },
       });
     }
-  }, [lat, lng]);
+  }, [currentMyCoordinates]);
 
   // 화장실 마커 + 클러스터링 적용
   useEffect(() => {
-    if (lat !== 0 && lng !== 0 && filterdToilets.length !== 0 && mapRef.current) {
+    if (lat !== 0 && lng !== 0 && toilets.length !== 0 && mapRef.current) {
       // 마커 클러스터링에 사용할 아이콘(HTML 마커)들을 정의
       const htmlMarker1 = {
         content:
@@ -95,14 +94,11 @@ export default function MainMap() {
       const markers: naver.maps.Marker[] = [];
       const infoWindows: naver.maps.InfoWindow[] = [];
 
-      for (let i = 0; i < filterdToilets.length; i++) {
-        let iconUrl: string = '/aroundToilet.png';
-        if (i === 0) iconUrl = '/closetToilet.png'; // 첫 번째(가장 가까운) 화장실 아이콘
-
+      toilets.forEach((toilet) => {
         const marker = new naver.maps.Marker({
-          position: new naver.maps.LatLng(filterdToilets[i].Y_WGS84, filterdToilets[i].X_WGS84),
+          position: new naver.maps.LatLng(toilet.Y_WGS84, toilet.X_WGS84),
           icon: {
-            url: iconUrl,
+            url: toilet.POI_ID === closestToilet.POI_ID ? '/closetToilet.png' : '/aroundToilet.png',
             size: new naver.maps.Size(35, 35),
             scaledSize: new naver.maps.Size(35, 35),
           },
@@ -112,8 +108,8 @@ export default function MainMap() {
         const infoWindow = new naver.maps.InfoWindow({
           content: `
             <div style="padding: 10px; box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 16px 0px;">
-              <div style="font-weight: bold; margin-bottom: 5px;">${filterdToilets[i].FNAME}</div>
-              <div style="font-size: 13px;">${filterdToilets[i].ANAME}</div>
+              <div style="font-weight: bold; margin-bottom: 5px;">${toilet.FNAME}</div>
+              <div style="font-size: 13px;">${toilet.ANAME}</div>
             </div>
           `,
           maxWidth: 300,
@@ -126,7 +122,7 @@ export default function MainMap() {
 
         markers.push(marker);
         infoWindows.push(infoWindow);
-      }
+      });
 
       // 2) 마커 클릭 시 정보창 열기/닫기
       markers.forEach((marker, i) => {
@@ -141,22 +137,22 @@ export default function MainMap() {
 
       // 3) MarkerClustering 생성
       const markerClustering = new MarkerClustering({
-        minClusterSize: 5, // 최소 몇 개부터 하나의 클러스터로 묶을지
-        maxZoom: 20, // 어디까지 확대하면 클러스터 해제하고 개별 마커를 보일지
         map: mapRef.current,
         markers,
         disableClickZoom: false,
-        gridSize: 120, //  마커를 클러스터로 인식하는 “범위”의 픽셀 크기
+        minClusterSize: 5,
+        maxZoom: 20,
+        gridSize: 150,
         icons: [htmlMarker1, htmlMarker2, htmlMarker3, htmlMarker4, htmlMarker5],
-        indexGenerator: [10, 100, 200, 500, 1000], // 클러스터 개수별로 어떤 아이콘을 표시할지
+        indexGenerator: [10, 100, 200, 500, 1000],
+        averageCenter: false,
         stylingFunction: (clusterMarker: any, count: any) => {
-          // clusterMarker.getElement()를 통해 실제 HTML 요소에 접근 가능
           const el = clusterMarker.getElement().firstChild as HTMLElement | null;
           if (el) el.textContent = String(count);
         },
       });
     }
-  }, [filterdToilets, currentMyCoordinates]);
+  }, [toilets, currentMyCoordinates]);
 
   return (
     <>
