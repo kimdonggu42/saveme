a78d01acdbd2a166c53e573e198796725e375d30
kimdonggu42 <kdk3280@undermilli.com>
2025-02-23 21:26:35 +0900

    chore: panorama 모듈 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
         {children}
         <Script
           type='text/javascript'
-          src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NAVER_MAP_API_KEY}&submodules=geocoder`}
+          src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NAVER_MAP_API_KEY}&submodules=geocoder,panorama`}
           strategy='beforeInteractive'
         />
         <Script src='/MarkerClustering.js' />
