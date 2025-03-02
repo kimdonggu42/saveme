60cca62841f38bda8a8bd645aa74ea48672a3ec4
kimdonggu42 <kdk3280@undermilli.com>
2025-03-03 01:42:52 +0900

    환경 변수 적용 테스트

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -4,7 +4,9 @@ const MAX_ROW_DATA_PER_CALL = 1000;
 
 const fetchToilets = async () => {
   try {
-    const totalCountRes = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/seoul-toilets/1/1`);
+    const totalCountRes = await fetch(
+      `https://saveme-git-feat-data-fetching-caching-kimdonggu42s-projects.vercel.app/api/seoul-toilets/1/1`,
+    );
     const parsedTotalCountRes = await totalCountRes.json();
     console.log(parsedTotalCountRes);
     const { list_total_count: totalCount } = parsedTotalCountRes.SearchPublicToiletPOIService;
@@ -14,7 +16,7 @@ const fetchToilets = async () => {
     for (let startRowNum = 1; startRowNum <= totalCount; startRowNum += MAX_ROW_DATA_PER_CALL) {
       const endRowNum = Math.min(startRowNum + MAX_ROW_DATA_PER_CALL - 1, totalCount);
       const res = await fetch(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/api/seoul-toilets/${startRowNum}/${endRowNum}`,
+        `https://saveme-git-feat-data-fetching-caching-kimdonggu42s-projects.vercel.app/api/seoul-toilets/${startRowNum}/${endRowNum}`,
       );
       const parsedRes = await res.json();
       promises.push(parsedRes);
