776e069ea37425ec4d8319a449d09b06efa31b87
kimdonggu42 <kdk3280@undermilli.com>
2025-03-04 23:41:41 +0900

    chore: eslint 설정 변경

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,21 @@
-export default {
-  env: {
-    es6: true,
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
   },
-  extends: 'next/core-web-vitals',
-  rules: {
-    'no-var': 'error',
-    'no-multiple-empty-lines': 'error',
-    'no-console': 'off',
-    eqeqeq: 'error',
-    'dot-notation': 'error',
-    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-  },
-};
+];
+
+export default eslintConfig;
