9c87e66c9244c958234f6436a9941ff38b1d5e15
kimdonggu42 <kdk3280@undermilli.com>
2025-03-07 21:49:29 +0900

    feat: geolocation 옵션 지정

--- src/hooks/useGeolocation.ts
+++ src/hooks/useGeolocation.ts
@@ -6,14 +6,17 @@ import { Coords } from '@/util/types';
 type GeolocationStatus = 'idle' | 'loading' | 'success' | 'error';
 
 const DEFAULT_COORDINATES = { lat: 37.5665, lng: 126.978 } as const;
+const options = {
+  enableHighAccuracy: false,
+  maximumAge: 30000,
+  timeout: 27000,
+} as const;
 
 export const useGeolocation = (onSuccess?: (coords: Coords) => void) => {
   const [currentMyCoordinates, setCurrentMyCoordinates] = useState<Coords | null>(null);
   const [geoStatus, setGeoStatus] = useState<GeolocationStatus>('idle');
 
   const getCurPosition = () => {
-    setGeoStatus('loading');
-
     const success = (location: { coords: { latitude: number; longitude: number } }) => {
       const newCoords = {
         lat: location.coords.latitude,
@@ -29,7 +32,13 @@ export const useGeolocation = (onSuccess?: (coords: Coords) => void) => {
       setGeoStatus('error');
     };
 
-    if (navigator.geolocation) navigator.geolocation.getCurrentPosition(success, error);
+    if (navigator.geolocation) {
+      setGeoStatus('loading');
+      navigator.geolocation.getCurrentPosition(success, error, options);
+    } else {
+      setGeoStatus('idle');
+      alert('브라우저가 위치 정보를 지원하지 않습니다.');
+    }
   };
 
   useEffect(() => {
