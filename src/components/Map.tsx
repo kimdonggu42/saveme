e5eed90a8e26fa784ed7608e3955eddd2908bcbc
kimdonggu42 <kdk3280@undermilli.com>
2025-03-09 01:19:00 +0900

    logging

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -222,7 +222,7 @@ export default function Map({ toilets }: MapProps) {
   const handleRequestLocationAccess = async () => {
     try {
       const permissionStatus = await navigator.permissions.query({ name: 'geolocation' });
-      console.log(permissionStatus.state);
+      alert(permissionStatus.state);
 
       if (permissionStatus.state !== 'denied') {
         getCurPosition();
