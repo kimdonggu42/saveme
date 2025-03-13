cab26303f524b1ca53cc057f15258d7900e37002
kimdonggu42 <kdk3280@undermilli.com>
2025-03-13 23:43:18 +0900

    style: toast 위치 조정

--- src/app/map/layout.tsx
+++ src/app/map/layout.tsx
@@ -14,7 +14,7 @@ export default function MapLayout({
         src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NAVER_MAP_API_KEY}&submodules=geocoder,panorama`}
       />
       <Script src='/MarkerClustering.js' />
-      <Toaster position='bottom-center' />
+      <Toaster position='top-center' offset={{ top: '70px' }} mobileOffset={{ top: '70px' }} />
     </>
   );
 }
